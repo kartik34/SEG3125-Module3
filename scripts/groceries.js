ba2f74bfe51a3f4c1025ab12d782053f7bb9c5b8
Kartik Singhal <kartiksinghal3.14@gmail.com>
2020-06-09 20:57:11 -0400

    finish requirements

--- scripts/groceries.js
+++ scripts/groceries.js
@@ -5,72 +5,92 @@ var products = [
 	{
 		name: "apple",
 		vegetarian: true,
-		glutenFree: false,
+		glutenFree: true,
 		organic: true,
-		price: 0.99
+		price: 0.99,
+		image: "https://upload.wikimedia.org/wikipedia/commons/0/07/Honeycrisp-Apple.jpg",
+		category: "fruitveg"
 	},
 	{
-		name: "brocoli",
+		name: "broccoli",
 		vegetarian: true,
 		glutenFree: true,
 		organic: true,
-		price: 1.99
+		price: 1.99,
+		image: "https://i5.walmartimages.ca/images/Enlarge/094/505/6000200094505.jpg",
+		category: "fruitveg"
 	},
 	{
 		name: "bread",
 		vegetarian: true,
 		glutenFree: false,
 		organic: false,
-		price: 2.35
+		price: 2.35,
+		image: "https://bakingamoment.com/wp-content/uploads/2020/01/IMG_7173-white-bread-2.jpg",
+		category: "breadcereals"
 	},
 	{
 		name: "eggs",
 		vegetarian: true,
 		glutenFree: true,
 		organic: false,
-		price: 2.49
+		price: 2.49,
+		image: "https://www.cdc.gov/features/salmonellaeggs/salmonellaeggs_1024px.jpg",
+		category: "protein"
 	},
 	{
 		name: "potato chips",
 		vegetarian: true,
 		glutenFree: true,
 		organic: false,
-		price: 2.49
+		price: 2.49,
+		image: "https://upload.wikimedia.org/wikipedia/commons/6/69/Potato-Chips.jpg",
+		category: "snacks"
 	},
 	{
 		name: "butter",
 		vegetarian: true,
 		glutenFree: true,
 		organic: true,
-		price: 4.99
+		price: 4.99,
+		image: "https://media.istockphoto.com/photos/butter-isolated-on-white-picture-id177834117?k=6&m=177834117&s=612x612&w=0&h=TWOC59dz208aXkSZ1M863ZzPHjV3XYq6FFWhgtH110I=",
+		category: "supplies"
 	},
 	{
 		name: "ham",
 		vegetarian: false,
 		glutenFree: true,
 		organic: false,
-		price: 5.99
+		price: 5.99,
+		image: "https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2019/11/spiced_ham.jpg",
+		category: "protein"
 	},
 	{
 		name: "milk",
 		vegetarian: false,
 		glutenFree: true,
 		organic: true,
-		price: 6.99
-	},
+		price: 6.99,
+		image: "https://dairyfarmersofcanada.ca/sites/default/files/image_file_browser/article/2019/getty-854296650.jpg",
+		category: "protein"
+	}, 
 	{
 		name: "salmon",
 		vegetarian: false,
 		glutenFree: true,
 		organic: true,
-		price: 9.99
+		price: 9.99,
+		image: "https://cdn.drweil.com/wp-content/uploads/2016/09/diet-nutrition_food-safety_worms-in-salmon_6922773-600x450.jpg",
+		category: "protein"
 	},
 	{
 		name: "cereal",
 		vegetarian: true,
 		glutenFree: false,
 		organic: false,
-		price: 9.99
+		price: 9.99,
+		image: "https://thumbor.thedailymeal.com/lfvreKMLztuCpXZDDF0aI570_VY=/870x565/https://www.thedailymeal.com/sites/default/files/2018/04/12/11_Cheerios_slide_Edit_1.jpg",
+		category: "breadcereals"
 	}
 	
 ];
@@ -122,5 +142,5 @@ function getTotalPrice(productPrices) {
 		totalPrice += productPrices[i];
 		
 	}
-	return totalPrice;
+	return totalPrice.toPrecision(4);
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 // This function is called when any of the tab is clicked
 // It is adapted from https://www.w3schools.com/howto/howto_js_tabs.asp
 
+
 function openInfo(evt, tabName) {
 
 	// Get all elements with class="tabcontent" and hide them
@@ -26,30 +27,65 @@ function openInfo(evt, tabName) {
 // generate a checkbox list from a list of products
 // it makes each product name as the label for the checkbos
 
-function populateListProductChoices(slct1, slct2) {
-    var s1 = document.getElementById(slct1);
-    var s2 = document.getElementById(slct2);
+function populateListProductChoices(slct1) {
+    var s1 = slct1;
+    var s2;
+
+    console.log(slct1);
 	
+	//clear divs first
+	document.getElementById("fruitveg").innerHTML = "";
+	document.getElementById("breadcereals").innerHTML = "";
+	document.getElementById("protein").innerHTML = "";
+	document.getElementById("snacks").innerHTML = "";
+	document.getElementById("supplies").innerHTML = "";
 	// s2 represents the <div> in the Products tab, which shows the product list, so we first set it empty
-    s2.innerHTML = "";
-		
+
 	// obtain a reduced list of products based on restrictions
-    var optionArray = restrictListProducts(products, s1.value);
+    var optionArray = restrictListProducts(products, s1);
+
 
 	// for each item in the array, create a checkbox element, each containing information such as:
 	// <input type="checkbox" name="product" value="Bread">
 	// <label for="Bread">Bread/label><br>
 		
 	for (i = 0; i < optionArray.length; i++) {
-			
+
 		var productName = optionArray[i];
+
+
+		var actualName = productName.substr(0, productName.length -6); 
+
+
+		var category = getCategory(products, actualName);
+		s2 = document.getElementById(category);
+
+		console.log(actualName);
+		console.log(category);
+		
+		// create image for the item
+
+
+		var url = getImageURL(products, actualName); 
+
+		console.log(url)
+
+		//this javascript code is modified from https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_img_create
+
+		var x = document.createElement("IMG");
+		x.setAttribute("src", url);
+		x.setAttribute("width", "80");
+		x.setAttribute("alt", actualName);
+		s2.appendChild(x);
+		
 		// create the checkbox and add in HTML DOM
 		var checkbox = document.createElement("input");
 		checkbox.type = "checkbox";
 		checkbox.name = "product";
 		checkbox.value = productName;
 		s2.appendChild(checkbox);
-		
+
+
 		// create a label for the checkbox, and also add in HTML DOM
 		var label = document.createElement('label')
 		label.htmlFor = productName;
@@ -58,6 +94,24 @@ function populateListProductChoices(slct1, slct2) {
 		
 		// create a breakline node and add in HTML DOM
 		s2.appendChild(document.createElement("br"));    
+	}
+}
+
+function getImageURL(products, product){
+	for (let i=0; i<products.length; i+=1) {
+		if(products[i].name == product){
+			return products[i].image;
+		}
+
+	}
+}
+
+function getCategory(products, product){
+	for (let i=0; i<products.length; i+=1) {
+		if(products[i].name == product){
+			return products[i].category;
+		}
+
 	}
 }
 	
@@ -80,6 +134,19 @@ function selectedItems(){
 	para.appendChild(document.createElement("br"));
 	for (i = 0; i < ele.length; i++) { 
 		if (ele[i].checked) {
+
+			//create image
+			var actualName = ele[i].value.substr(0, ele[i].value.length -6); 
+			var url = getImageURL(products, actualName); 
+
+			//this javascript code is modified from https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_img_create
+
+			var x = document.createElement("IMG");
+			x.setAttribute("src", url);
+			x.setAttribute("width", "80");
+			x.setAttribute("alt", actualName);
+			para.appendChild(x);
+
 			para.appendChild(document.createTextNode(ele[i].value));
 			para.appendChild(document.createElement("br"));
 			chosenProducts.push(ele[i].value);
@@ -93,3 +160,31 @@ function selectedItems(){
 	c.appendChild(document.createTextNode("Total Price is $" + getTotalPrice(priceProducts)));
 		
 }
+
+//following javascript code is from https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_accordion
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+//following javascript code is from https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_sidenav
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
